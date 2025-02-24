b35ae83488d8f621e58bc160186a481383395f3c
MrunaliKamble19 <mrunalikamble2008@gmail.com>
2025-02-24 15:11:32 +0530

    3 commit

--- clothing/views.py
+++ clothing/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from clothing.models import product
 from .forms import ProductForm
 
@@ -15,3 +15,27 @@ def clothing(request):
             form = ProductForm() # Reset form after saving
             
     return render(request, 'clothing/fashion.html',{'pro_img': pro_img, 'form':form})
+
+
+def edit_product(request, product_id):
+    prod = get_object_or_404(product, id=product_id)
+    
+    if request.method == "POST":
+        form = ProductForm(request.POST, request.FILES, instance=prod)
+        if form.is_valid():
+            form.save()
+            return redirect('clothing')
+    else:
+        form = ProductForm(instance=prod)
+        
+    return render(request, 'clothing/edit_product.html', {'form': form})
+
+
+def delete_product(request, product_id):
+    prod = get_object_or_404(product, id=product_id)
+    
+    if request.method == "POST":
+        prod.delete()
+        return redirect('clothing')
+        
+    return render(request, 'clothing/confirm_delete.html', {'product': prod})
